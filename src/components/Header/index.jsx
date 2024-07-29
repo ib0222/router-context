a78b81cf5b0b50eb2335f4b0c71cee4f00dcd1fb
ib0222 <ibrahim.ismayilzade221@gmail.com>
2024-07-30 02:00:52 +0400

    card styling

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,27 +1,32 @@
-import { Link } from "react-router-dom";
-import React from "react";
-
+import { useContext,React } from "react";
+import { NavLink } from "react-router-dom";
+import { CardContext } from "../../context/CardContext";
 const Header = () => {
+  const { favorites } = useContext(CardContext);
+
   return (
     <div className="w-full h-20 bg-slate-500 flex items-center gap-10 justify-center">
-      <Link
-        className="bg-slate-800 p-2 text-white rounded-xl hover:bg-slate-600 ease-in-out duration-200"
+      <NavLink
+        className="bg-slate-800 p-4 text-white rounded-xl hover:bg-slate-600 ease-in-out duration-200"
         to={"/"}
       >
         Home
-      </Link>
-      <Link
-        className="bg-slate-800  text-white p-2 rounded-xl hover:bg-slate-600 ease-in-out duration-200"
+      </NavLink>
+      <NavLink
+        className="bg-slate-800  text-white p-4 rounded-xl hover:bg-slate-600 ease-in-out duration-200"
         to={"/products"}
       >
         Products
-      </Link>
-      <Link
-        className="bg-slate-800  text-white p-2 rounded-xl hover:bg-slate-600 ease-in-out duration-200"
+      </NavLink>
+      <NavLink
+        className="bg-slate-800  text-white p-4 rounded-xl hover:bg-slate-600 ease-in-out duration-200"
         to={"/favorites"}
       >
         Favorites
-      </Link>
+        <span className=" text-red-600 ml-2">
+        ({favorites.length})
+        </span>
+      </NavLink>
     </div>
   );
 };
--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -1,9 +1,19 @@
 import React, { useContext } from "react"
 import { CardContext } from "../../context/CardContext"
-
+import Card from "../../components/Card"
 const Favorites = () => {
   const { favorites, setFavorites } = useContext(CardContext)
-  return <div>{JSON.stringify(favorites)}</div>
+  return <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mx-20 gap-10 my-4 ">{
+      favorites.map((item) => (
+        <Card
+            key={item.id}
+            id={item.id}
+            image={item.image}
+            title={item.title}
+            price={item.price}
+          />
+      ))
+    }</div>
 }
 
 export default Favorites
