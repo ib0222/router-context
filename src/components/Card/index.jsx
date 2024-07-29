65fa57817246b3202af4e7dd2a14195281d4721a
Tamerlan Kengerli <tamerlankengerli2002@gmail.com>
2024-07-30 02:36:05 +0400

    fix card component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react"
+import { useContext } from "react"
 import { CardContext } from "../../context/CardContext"
 import { IoIosHeartEmpty, IoMdHeart } from "react-icons/io"
 
@@ -15,27 +15,30 @@ const Card = ({ id, image, title, price }) => {
   }
 
   return (
-    <div className="relative border p-3 rounded-xl bg-slate-300 hover:bg-slate-500 ease-in-out duration-300 ">
-      <div className="flex justify-end z-50">
-        <button className="bg-white p-1 rounded-full" onClick={handleFavorite}>
-          {favorites.findIndex((item) => item.id === id) !== -1 ? (
-            <IoMdHeart className="size-5 text-red-700" />
-          ) : (
-            <IoIosHeartEmpty className="size-5 text-red-700" />
-          )}
-        </button>
-      </div>
-      <div className="flex justify-center w-">
+    <div className="relative border p-3 rounded-xl bg-slate-300 hover:bg-slate-500 ease-in-out duration-300 flex flex-col min-h-[400px]">
+      <div className="flex-1 flex justify-center items-center h-1/2">
         <img
           src={image}
-          alt=""
-          className="rounded object-contain w-24 mix-blend-multiply"
+          alt={title}
+          className="rounded object-contain mix-blend-multiply w-full h-full"
         />
       </div>
-      <div className="text-center mt-3 font-bold">
-        <h3>{title}</h3>
+      <div className="flex-1 text-center font-bold flex flex-col justify-center items-center h-1/2">
+        <h3 className="text-lg font-semibold tracking-wide text-slate-800 line-clamp-2">
+          {title}
+        </h3>
+        <p className="text-lg mt-2 text-slate-700">{price} $</p>
       </div>
-      <div className="text-center mt-3">{price} $</div>
+      <button
+        className="bg-white p-1 rounded-full absolute right-3 top-3"
+        onClick={handleFavorite}
+      >
+        {favorites.findIndex((item) => item.id === id) !== -1 ? (
+          <IoMdHeart className="text-red-700" />
+        ) : (
+          <IoIosHeartEmpty className="text-red-700" />
+        )}
+      </button>
     </div>
   )
 }
--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect, useContext } from "react"
+import React, { useEffect, useState } from "react"
 import Card from "../../components/Card"
 import Loading from "../../components/Loading"
-import { CardContext } from "../../context/CardContext"
 
 const Products = () => {
   const [data, setData] = useState([])
@@ -13,11 +12,9 @@ const Products = () => {
 
   return (
     <div>
-      <h1 className="text-center text-4xl my-6 font-semibold">
-        All products
-      </h1>
+      <h1 className="text-center text-4xl my-6 font-semibold">All products</h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mx-20 gap-10 my-4 ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mx-4 lg:mx-20 gap-10 my-4 ">
         {data.length === 0 ? (
           <Loading />
         ) : (
